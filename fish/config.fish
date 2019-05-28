3666acd1fded4ff4326cd1747e934a5b4f24e135
Rohan Yadav <rohany@alumni.cmu.edu>
2019-05-28 15:30:28 -0400

    test pr

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
-set -x DIDEROT_HOME "/home/rohany/Documents/lambdabricks/src"
-set -x PYTHONPATH "$DIDEROT_HOME:$PYTHONPATH"
-alias andrew "cd /afs/andrew.cmu.edu/usr3/rohany/"
-alias cs "cd /afs/cs.cmu.edu/user/rohany"
+# set -x DIDEROT_HOME "/home/rohany/Documents/lambdabricks/src"
+# set -x PYTHONPATH "$DIDEROT_HOME:$PYTHONPATH"
+# alias andrew "cd /afs/andrew.cmu.edu/usr3/rohany/"
+# alias cs "cd /afs/cs.cmu.edu/user/rohany"
 alias vim "nvim"
 
 # set PATH $PATH /Users/rohany/cc0/bin/
-set PATH $PATH /usr/local/opt/llvm@6/bin/
-set PATH $PATH /usr/local/opt/qt/bin/
+# set PATH $PATH /usr/local/opt/llvm@6/bin/
+# set PATH $PATH /usr/local/opt/qt/bin/
 
-export PATH
+# export PATH
 # OPAM configuration
-source /Users/rohany/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+# source /Users/rohany/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 mkdir -p ~/.config/fish/
 mkdir -p ~/.config/nvim/
 
+echo "setting up"
+
 # copy in files
 ln -sf fish/config.fish ~/.config/fish/config.fish
 ln -sf nvim/init.vim ~/.config/nvim/init.vim
